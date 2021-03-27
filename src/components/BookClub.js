6dc88c8f853b149960f7d8682dc3c0630cb25231
florapetersen <floramarina@comcast.net>
2021-03-27 09:02:02 -0700

    update book club component

--- src/components/BookClub.js
+++ src/components/BookClub.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import UserBookClubFormContainer from '../containers/UserBookClubFormContainer'
 
 const BookClub = ({ bookClub }) => {
-    console.log(bookClub)
     return (
         <div> 
             <p><h1>{bookClub.name}</h1></p>
             <p><h1>Category: {bookClub.category}</h1>
             <h1>About us: {bookClub.description}</h1></p>
+            <Link to={UserBookClubFormContainer}>Join this club!</Link>
         </div>
     )
 }
