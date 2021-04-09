5a9c457f0c607c0b139995153284e68fd7d63baa
florapetersen <floramarina@comcast.net>
2021-04-08 20:23:20 -0700

    update link to /userBookClubs/new

--- src/components/BookClub.js
+++ src/components/BookClub.js
@@ -7,7 +7,7 @@ const BookClub = ({ bookClub }) => {
             <p><h1>{bookClub.name}</h1></p>
             <p><h1>Category: {bookClub.category}</h1>
             <h1>About us: {bookClub.description}</h1></p>
-            <Link to={`/userBookClubs/new}`}>Join this club!</Link>
+            <Link to={`/userBookClubs/new}`} bookClub={bookClub}>Join this club!</Link>
         </div>
     )
 }
