8723a2e4da4db9aeec2a829c297837a99d9a1426
florapetersen <floramarina@comcast.net>
2021-04-15 17:01:05 -0700

    add useEffect method to Flash.js

--- src/components/Flash.js
+++ src/components/Flash.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Bus from '../Utils/Bus';
 import './index.css';
 
 export const Flash = () => {
@@ -7,6 +8,24 @@ export const Flash = () => {
     let [message, setMessage] = useState('');
     let [type, setType] = useState('');
 
+    useEffect(() => {
+        Bus.addListener('flash', ({message, type}) => {
+            setVisibility(true);
+            setMessage(message);
+            setType(type);
+            setTimeout(() => {
+                setVisibility(false);
+            }, 4000);
+        });
+    }, []);
+
+    useEffect(() => {
+        if(document.querySelector('.close') !== null) {
+            document.
+            querySelector('.close').
+            addEventListener('click', () => setVisibility(false));
+        }
+    })
 
     return (
         visibility && <div className={`alert alert-${type}`}>
