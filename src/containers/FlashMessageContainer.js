6ba573dc6f1f5f29f6aa81c2feb1293f918473b6
florapetersen <floramarina@comcast.net>
2021-04-21 14:24:49 -0700

    update FlashMessageContainer.js

---
+++ src/containers/FlashMessageContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FlashMessage extends Component {
+    state = {
+        message = ""
+    }
+
+    render() {
+
+    }
+}
+
+export default FlashMessage
