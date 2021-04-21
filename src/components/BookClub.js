b8929acbb4b80ed7976367ca6277d3de996dbc43
florapetersen <floramarina@comcast.net>
2021-04-20 18:13:31 -0700

    replaced handleClick in BookClub.js

--- src/components/BookClub.js
+++ src/components/BookClub.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import {createUserBookClub} from "../actions/userBookClubs"
+import Flash from './Flash'
 
 const BookClub = ({ bookClub, dispatchCreateUserBookClub }) => {
+
+    function handleClick(e) {
+        dispatchCreateUserBookClub(bookClub);
+        <Flash bookClub={bookClub}/>
+    }
 
     return (
         <div> 
             <p><h1>{bookClub.name}</h1></p>
             <p><h1>Category: {bookClub.category}</h1>
             <h1>About us: {bookClub.description}</h1></p>
-            <button onClick={() => dispatchCreateUserBookClub(bookClub)}>Join this club!</button>
+            <button onClick={handleClick()}>Join this club!</button>
         </div>
     )
 }
