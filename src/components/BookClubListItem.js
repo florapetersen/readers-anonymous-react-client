f00d2a6917c44fe10368fe2e4fd0c18332d3d0d4
florapetersen <floramarina@comcast.net>
2021-06-22 19:04:47 -0700

    added star feature to bookclublistitem.js

--- src/components/BookClubListItem.js
+++ src/components/BookClubListItem.js
@@ -2,14 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { deleteBookClub } from '../actions/bookClubs'
+import { Component } from 'react';
 
-const BookClubListItem = ({ bookClub, dispatchDeleteBookClub }) => {
-    return (
-        <li className="" key={bookClub.id}>
-            <Link to={`/bookClubs/${bookClub.id}`}>{bookClub.name} </Link>
-            <button onClick={() => dispatchDeleteBookClub(bookClub.id)}> DELETE</button>
+class BookClubListItem extends Component {
+
+    state = {
+        stars: 0
+    }
+
+    addStar = () => {
+        this.setState({stars: this.state.stars + 1})
+    }
+
+    render() {
+        return (
+        <li className="" key={this.props.bookClub.id}>
+            <Link to={`/bookClubs/${this.props.bookClub.id}`}>{this.props.bookClub.name} </Link>
+            <p><button onClick={() => this.props.dispatchDeleteBookClub(this.props.bookClub.id)}> DELETE</button></p>
+            <p><button onClick={this.addStar}>Add a Star</button></p>
+            <p>Stars: {this.state.stars}</p>
         </li>
     )
+    }
 }
 
 const mapDispatchToProps = (dispatch) => {
