8067b2da72ed84fff08cd11eb1d7525eab6ef3e5
florapetersen <floramarina@comcast.net>
2021-06-26 23:23:56 -0700

    update BookClub.js

--- src/components/BookClub.js
+++ src/components/BookClub.js
@@ -7,13 +7,16 @@ import { FlashProviderContext } from "react-simple-widgets";
 
 const BookClub = ({ bookClub, dispatchCreateUserBookClub, dispatchAddFlashMessage }) => {
 
+    const { flashError, flashWarning, flashInfo, flashSuccess } = useContext(FlashProviderContext);
+    const message = "You have joined this book club!";
+
     function handleClick(e) {
         dispatchCreateUserBookClub(bookClub); //adds UserBookClub object to database 
         //dispatchAddFlashMessage({ //calls dispatchAddFlashMessage function in action creator 
             //type: 'success', //function has two parameters: type and text
             //text: 'You have joined this book club'
         //});
-
+        flashSuccess("Success", <u>{message}</u>, () => alert("Success flash message"))
     }
 
     return (
@@ -21,7 +24,11 @@ const BookClub = ({ bookClub, dispatchCreateUserBookClub, dispatchAddFlashMessag
             <p><h1>{bookClub.name}</h1></p>
             <p><h1>Category: {bookClub.category}</h1>
             <h1>About us: {bookClub.description}</h1></p>
-            <button onClick={handleClick()}>Join this club!</button>
+            <button 
+                className="btn btn-success btn-sm"
+                onClick={handleClick()}>
+                Join this club!
+            </button>
         </div>
     )
 }
@@ -32,5 +39,6 @@ const mapDispatchToProps = (dispatch) => {
         dispatchAddFlashMessage: (message) => dispatch(addFlashMessage(message))
     }
 }
+
 
 export default connect(null, mapDispatchToProps)(BookClub)
