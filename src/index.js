55ecb602d54c838ea78229b603f2470b12f7cd14
florapetersen <floramarina@comcast.net>
2021-06-26 22:21:01 -0700

    add FlashProvider and DialogProvider to index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import App from './App';
 import store from './store'
+import { FlashProvider, DialogProvider } from "react-simple-widgets";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <DialogProvider>
+        <FlashProvider >
+          <App />
+        </FlashProvider>
+      </DialogProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
