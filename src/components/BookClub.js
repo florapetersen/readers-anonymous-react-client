f94c856e3de421d534c32966c3de7e5ba1fae050
florapetersen <floramarina@comcast.net>
2021-06-26 23:11:57 -0700

    imported useContext and FlashProviderContext to BookClub.js

--- src/components/BookClub.js
+++ src/components/BookClub.js
@@ -2,15 +2,18 @@ import React from 'react'
 import { connect } from 'react-redux'
 import {createUserBookClub} from "../actions/userBookClubs"
 import {addFlashMessage} from "../actions/flash"
+import { useContext } from "react";
+import { FlashProviderContext } from "react-simple-widgets";
 
 const BookClub = ({ bookClub, dispatchCreateUserBookClub, dispatchAddFlashMessage }) => {
 
     function handleClick(e) {
-        dispatchCreateUserBookClub(bookClub);
-        dispatchAddFlashMessage({
-            type: 'success',
-            text: 'You have joined this book club'
-        });
+        dispatchCreateUserBookClub(bookClub); //adds UserBookClub object to database 
+        //dispatchAddFlashMessage({ //calls dispatchAddFlashMessage function in action creator 
+            //type: 'success', //function has two parameters: type and text
+            //text: 'You have joined this book club'
+        //});
+
     }
 
     return (
@@ -29,6 +32,5 @@ const mapDispatchToProps = (dispatch) => {
         dispatchAddFlashMessage: (message) => dispatch(addFlashMessage(message))
     }
 }
-
 
 export default connect(null, mapDispatchToProps)(BookClub)
